85966b2a300fd49b264328e67014a7075144e7a9
VLAILIN <54513099+VLAILIN@users.noreply.github.com>
2019-09-12 15:52:03 +0300

    Update Rect.h

--- Lab2/Rect.h
+++ Lab2/Rect.h
@@ -3,31 +3,12 @@
 class Rect:public Shape
 {
 private:
-	int m_left, m_top, m_right, m_bottom; //координаты двух точек прямоугольника
+	int m_left, m_top, m_right, m_bottom; //РєРѕРѕСЂРґРёРЅР°С‚С‹ РґРІСѓС… С‚РѕС‡РµРє РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР°
 public:
-	Rect(int x1=0, int y1=0, int x2=0, int y2=0, COLORS C=WHITE); //конструктор с параметрами
-	//Rect(const Rect &obj); // конструктор копирования
-	virtual void WhereAmI() const; //вывод сообщения
-	void GetAll(int&, int&, int&, int&) const; //функция вытягивания значений private-переменных класса
+	Rect(int x1=0, int y1=0, int x2=0, int y2=0, COLORS C=WHITE); //РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ СЃ РїР°СЂР°РјРµС‚СЂР°РјРё
+	//Rect(const Rect &obj); // РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РєРѕРїРёСЂРѕРІР°РЅРёСЏ
+	virtual void WhereAmI() const; //РІС‹РІРѕРґ СЃРѕРѕР±С‰РµРЅРёСЏ
+	void GetAll(int&, int&, int&, int&) const; //С„СѓРЅРєС†РёСЏ РІС‹С‚СЏРіРёРІР°РЅРёСЏ Р·РЅР°С‡РµРЅРёР№ private-РїРµСЂРµРјРµРЅРЅС‹С… РєР»Р°СЃСЃР°
 	void Inflate(int);
-	virtual ~Rect(); //деструктор класса
+	virtual ~Rect(); //РґРµСЃС‚СЂСѓРєС‚РѕСЂ РєР»Р°СЃСЃР°
 };
-
-
-
-/*
-private:
-	 m_left, m_right, m_top, m_bottom; //координаты прямоугольника
-public:
-	Rect(); //конструктор по умолчанию
-	Rect(int, int, int, int); //конструктор с параметрами
-	Rect(const Rect &obj); // конструктор копирования
-	void InflateRect(int, int, int, int); //функция расширения прямоугольника
-	void InflateRect(int=1, int=1); //функция расширения прямоугольника с двумя параметрами по умолчанию
-	void SetAll(int, int, int, int); //установка значений координат прямоугольника
-	void GetAll(int&, int&, int&, int&) const; //функция вытягивания значений private-переменных класса
-	void BoundingRect(const Rect&, const Rect&);
-	~Rect(); //деструктор класса
-protected:
-	void Normalize(); //нормализация прямоугольника
-*/
